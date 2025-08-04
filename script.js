4a228b7c993521d6ad1ed4632995e27e72ad023e
Shalama <shalamafadillaa@gmail.com>
2025-08-04 13:39:51 +0700

    Creata a variable to store a reference
    Create it to store the reference of the result text and buttons.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create a variable to store the reference of the result text and buttons
+const displayResult = document.querySelector("#result-text");
+const digitBtn = document.querySelectorAll(".digit");
+const featureBtn = document.querySelectorAll(".feature-btn");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const equalBtn = document.querySelector("#equal-btn");
+
