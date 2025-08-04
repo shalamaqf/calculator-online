be9fa28d77574d638415306b70fa3953caacf8c6
Shalama <shalamafadillaa@gmail.com>
2025-08-04 14:04:43 +0700

    Create a function to display the digit
    Add event listeners to every digit buttons and store the input to a string variable and display it with display result variable.

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const equalBtn = document.querySelector("#equal-btn");
 
 // Create a string variable to store the user input
 let input = "";
+
+/*
+ * Create a function to add event listeners to digit buttons
+ * When a digit button clicked, it will stored in a string variable
+ * Then, the string variable will be displayed in the result text
+*/
+function displayDigit() {
+    digitBtn.forEach(digit => {
+    digit.addEventListener('pointerdown', () => {
+        input += digit.textContent;
+        displayResult.textContent = input;
+    })
+})
